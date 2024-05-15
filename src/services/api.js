353e070f7212f115f0f2e8cccdf9d91ceb35b598
Alec Lima <aleclimadev@gmail.com>
2024-05-15 20:52:10 -0300

    feat: interceptors

---
+++ src/services/api.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: 'https://api.homologation.cliqdrive.com.br/auth',
+}); 
+
+api.interceptors.request.use( async (config) => {
+    const bearerToken = import.meta.env.TOKEN
+    config.headers.Authorization = `Bearer ${bearerToken}`;
+    config.headers.Accept = 'application/json;version=v1_web';
+    config.headers["Content-Type"] = 'application/json';
+
+    const dataLocalStorage = config.data
+
+    if (dataLocalStorage) {
+        localStorage.setItem("email", dataLocalStorage.email);
+        localStorage.setItem("password", dataLocalStorage.password);
+        localStorage.getItem("token") !== undefined ? localStorage.setItem("token", bearerToken) : false 
+       
+        return {...config, data: dataLocalStorage};
+    }
+  
+ 
+    return config;
+}, (error) => {
+    return console.log(error);
+});
+
+export default api;
