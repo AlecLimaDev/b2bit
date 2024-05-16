a959b6e4d54e7dd8f34796a3bd4d578e5da470d8
Alec Lima <aleclimadev@gmail.com>
2024-05-16 19:51:04 -0300

    feat: vitest config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./setupVitest.js"],
+    include: ['**/(*.)?{test,spec}.{js,mjs,cjs,mts,cts,jsx}'],
+  },
 })
