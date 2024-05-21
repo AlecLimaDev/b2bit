a653f1fd28f0067904f13f830fc848093eba478e
Alec Lima <aleclimadev@gmail.com>
2024-05-21 09:58:38 -0300

    perf:  profile request

--- src/pages/Home/view.model.js
+++ src/pages/Home/view.model.js
@@ -5,12 +5,20 @@ export default function HomeViewModel() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
+    const controller = new AbortController();
     async function fetchData() {
-      const responseApi = await profileService();
+      const responseApi = await profileService({
+        controller: controller.signal,
+      });
       setData(responseApi);
     }
 
     fetchData();
+
+    return () => {
+      controller.abort();
+    };
+    
   }, []);
 
   const filterData = { ...data };
